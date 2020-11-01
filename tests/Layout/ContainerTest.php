9c2b392956b4679536aba43dd3de066d18002a1e
Christophedlr <christophedlr@gmail.com>
2020-11-01 20:47:30 +0100

    Feature(Layout): Tests of Container tag
    - Test container with type attribute
    - Test container with class attribute
    - Test container with type & class attribute
    - Test container with invalid attribute

--- tests/Layout/ContainerTest.php
+++ tests/Layout/ContainerTest.php
@@ -3,10 +3,10 @@
 
 namespace Layout;
 
-
 use Bootstrap\Extension\Bootstrap4Extension;
 use PHPUnit\Framework\TestCase;
 use Twig\Environment;
+use Twig\Error\SyntaxError;
 use Twig\Loader\FilesystemLoader;
 
 class ContainerTest extends TestCase
@@ -29,10 +29,77 @@ class ContainerTest extends TestCase
         $this->template->addExtension(new Bootstrap4Extension());
     }
 
+    /**
+     * {% container %}
+     * {% endcontainer %}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function testContainer()
     {
         $result = $this->template->render('layout/container.html');
 
         $this->assertEquals("<div class=\"container\"></div>", $result);
+    }
+
+    /**
+     * {% container type="fluid" %}
+     * {% endcontainer %}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function testContainerFluid()
+    {
+        $result = $this->template->render('layout/container_fluid.html');
+
+        $this->assertEquals("<div class=\"container-fluid\"></div>", $result);
+    }
+
+    /**
+     * {% container class="custom-container" %}
+     * {% endcontainer %}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function testContainerCustomContainerClass()
+    {
+        $result = $this->template->render('layout/container_class.html');
+
+        $this->assertEquals("<div class=\"container custom-container\"></div>", $result);
+    }
+
+    /**
+     * {% container class,type = "custom-container","xl" %}
+     * {% endcontainer %}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function testContainerXlCustomContainerClass()
+    {
+        $result = $this->template->render('layout/container_xl_class.html');
+
+        $this->assertEquals("<div class=\"container-xl custom-container\"></div>", $result);
+    }
+
+    /**
+     * {% container attribute="test" %}
+     * {% endcontainer %}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function testContainerInvalidAttribute()
+    {
+        $this->expectException(SyntaxError::class);
+        $result = $this->template->render('layout/container_invalid.html');
     }
 }
