1304328d4b4481a0dd1e3d75941ddadb182556a8
Christophedlr <christophedlr@gmail.com>
2020-11-03 10:41:02 +0100

    Feature(Components): Breadcrumb function

--- src/Extension/Bootstrap4Extension.php
+++ src/Extension/Bootstrap4Extension.php
@@ -8,6 +8,7 @@ use Bootstrap\TokenParser\Bootstrap4\ColTokenParser;
 use Bootstrap\TokenParser\Bootstrap4\ContainerTokenParser;
 use Bootstrap\TokenParser\Bootstrap4\RowTokenParser;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 class Bootstrap4Extension extends AbstractExtension
 {
@@ -19,5 +20,28 @@ class Bootstrap4Extension extends AbstractExtension
             new ColTokenParser(),
             new BreadcrumbTokenParser(),
         ];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('breadcrumb', [$this, 'breadcrumbFunction'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function breadcrumbFunction(string $text, $url = '', bool $active = false)
+    {
+        $value = $text;
+
+        if (!empty($url) && !$active) {
+            $value = sprintf('<a href="%s">%s</a>', $url, $text);
+        }
+
+        return sprintf(
+            "<li class=\"breadcrumb-item%s\"%s>%s</li>",
+            ($active)?' active':'',
+            ($active)?' aria-current="page"':'',
+            $value
+        );
     }
 }
