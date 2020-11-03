07fd6e44e5af68b086278692ef68f42fb007410d
Christophedlr <christophedlr@gmail.com>
2020-11-03 10:40:47 +0100

    Feature(Components): Tests of Breadcrumb function

--- tests/Components/BreadcrumbTest.php
+++ tests/Components/BreadcrumbTest.php
@@ -80,4 +80,88 @@ class BreadcrumbTest extends TestCase
         $this->expectException(SyntaxError::class);
         $result = $this->template->render('components/breadcrumb/tag_invalid.html');
     }
+
+    /**
+     * {{ breadcrumb('Link') }}
+     *
+     * @throws SyntaxError
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     */
+    public function testBreadcrumbFunction()
+    {
+        $expected = '<li class="breadcrumb-item">Link</li>';
+        $result = $this->template->render('components/breadcrumb/function.html');
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * {{ breadcrumb('Link', '#') }}
+     *
+     * @throws SyntaxError
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     */
+    public function testBreadcrumbFunctionLink()
+    {
+        $expected = '<li class="breadcrumb-item"><a href="#">Link</a></li>';
+        $result = $this->template->render('components/breadcrumb/function_link.html');
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * {{ breadcrumb('Link', '', true) }}
+     *
+     * @throws SyntaxError
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     */
+    public function testBreadcrumbFunctionActive()
+    {
+        $expected = '<li class="breadcrumb-item active" aria-current="page">Link</li>';
+        $result = $this->template->render('components/breadcrumb/function_active.html');
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * {{ breadcrumb() }}
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \ArgumentCountError
+     */
+    public function testBreadcrumbFunctionNoParams()
+    {
+        $this->expectException(\ArgumentCountError::class);
+        $result = $this->template->render('components/breadcrumb/function_invalid.html');
+    }
+
+    /**
+     * {% breadcrumb %}
+     * {{ breadcrumb('Home', '#') }}
+     * {{ breadcrumb('Library', '#') }}
+     * {{ breadcrumb('Data') }}
+     * {% endbreadcrumb %}
+     *
+     * @throws SyntaxError
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     */
+    public function testFullBreadcrumb()
+    {
+        $expected = "<nav aria-label=\"breadcrumb\">";
+        $expected .= "<ol class=\"breadcrumb\">";
+        $expected .= "<li class=\"breadcrumb-item\"><a href=\"#\">Home</a></li>\n";
+        $expected .= "<li class=\"breadcrumb-item\"><a href=\"#\">Library</a></li>\n";
+        $expected .= "<li class=\"breadcrumb-item active\" aria-current=\"page\">Data</li>\n";
+        $expected .= "</ol>";
+        $expected .= "</nav>";
+
+        $result = $this->template->render('components/breadcrumb/breadcrumb.html');
+
+        $this->assertEquals($expected, $result);
+    }
 }
